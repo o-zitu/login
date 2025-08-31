e44ec68b43bf79fdea223badb846443b4e6d1c51
Vitor de Oliveira Limberger <vitorzituvl@gmail.com>
2025-08-31 02:44:07 -0300

    First Commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export function App() {
+    const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+    </>
+  )
+}
